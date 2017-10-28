edfc6ef3f427b51bb805b2af04e33dd3cc5c69ff
Lalit Adithya <lalitadithya5071@gmail.com>
2017-10-28 09:35:35 +0530

    Template for student bulk upload.

---
+++ PreOTAS/Controllers/TemplateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PreOTAS.Controllers
+{
+    public class TemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult GetTemplateForStudent()
+        {
+            string filePath = Server.MapPath("~/Templates/Student.xlsx");
+            FileInfo file = new FileInfo(filePath);
+            Response.Clear();
+            Response.ClearHeaders();
+            Response.ClearContent();
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name);
+            Response.AddHeader("Content-Length", file.Length.ToString());
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            Response.Flush();
+            Response.TransmitFile(file.FullName);
+            Response.End();
+            return View();
+        }
+    }
+}
